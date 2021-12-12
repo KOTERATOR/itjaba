0d76b2719a9339e6cb3da2df7d19f28893700d0e
Илья <koteskateboarder@gmail.com>
2021-12-12 23:17:31 +0300

    Lab.2.1

--- Lab.2.1/src/ru/mirea/it/Main.java
+++ Lab.2.1/src/ru/mirea/it/Main.java
@@ -1,5 +1,7 @@
 package ru.mirea.it;
 
+import java.util.Scanner;
+
 public class Main {
 
     private static int task5_9(int a, int b, int prev) {
@@ -23,8 +25,30 @@ public class Main {
         }
     }
 
+    private static void task5_15(int number) {
+        if (number == 0)
+            System.out.println(number + " ");
+        else {
+            System.out.print((number % 10) + " ");
+            number = number / 10;
+            if (number != 0)
+                task5_15(number);
+        }
+    }
+
+    private static int task5_19()
+    {
+        Scanner s = new Scanner(System.in);
+        int value = s.nextInt();
+        if (value == 0)
+            return 0;
+        int nextMax = task5_19();
+        return Math.max(nextMax, value);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(task5_19());
+	    task5_15(0);
         System.out.println(task5_9(1, 3, -1));
 
         new Task4().setVisible(true);
